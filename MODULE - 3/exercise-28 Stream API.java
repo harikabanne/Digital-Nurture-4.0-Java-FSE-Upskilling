4a6e1a71337dfb00f36559c5f0e6ec6dd0722c64
harikabanne <harikabanne@gmail.com>
2025-05-27 14:20:49 +0530

    Create exercise-28 Stream API.java

---
+++ MODULE - 3/exercise-28 Stream API.java
@@ -0,0 +1,10 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class StreamEven {
+    public static void main(String[] args) {
+        List<Integer> numbers = Arrays.asList(10, 15, 20, 25, 30);
+        List<Integer> even = numbers.stream().filter(n -> n % 2 == 0).collect(Collectors.toList());
+        System.out.println("Even numbers: " + even);
+    }
+}
