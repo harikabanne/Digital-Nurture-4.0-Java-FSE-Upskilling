1e69fcf37a5780eb214286266982630df88eb045
harikabanne <harikabanne@gmail.com>
2025-05-27 14:11:01 +0530

    Create exercise-16 Palindrome Checker.java

---
+++ MODULE - 3/exercise-16 Palindrome Checker.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class PalindromeChecker {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String input = scanner.nextLine();
+
+        String cleaned = input.replaceAll("[^A-Za-z0-9]", "").toLowerCase();
+        String reversed = new StringBuilder(cleaned).reverse().toString();
+
+        if (cleaned.equals(reversed)) {
+            System.out.println("Palindrome");
+        } else {
+            System.out.println("Not a palindrome");
+        }
+
+        scanner.close();
+    }
+}
