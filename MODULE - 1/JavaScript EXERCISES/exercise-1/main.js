821a636e04b06d1c9fc576998f1d3c8257b5e2cf
harikabanne <harikabanne@gmail.com>
2025-05-27 15:45:48 +0530

    Create main.js

---
+++ MODULE - 1/JavaScript EXERCISES/exercise-1/main.js
@@ -0,0 +1,7 @@
+// Log a welcome message in console
+console.log("Welcome to the Community Portal");
+
+// Alert when page is fully loaded
+window.onload = function() {
+    alert("Page is fully loaded!");
+};
