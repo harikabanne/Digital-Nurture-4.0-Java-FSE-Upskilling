dce7b44437571b80cb30d2f160e390f60687c37b
harikabanne <harikabanne@gmail.com>
2025-05-27 14:07:15 +0530

    Create exercise-10 Number Guessing Game.java

---
+++ MODULE - 3/exercise-10 Number Guessing Game.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+import java.util.Random;
+
+public class NumberGuessingGame {
+    public static void main(String[] args) {
+        Random random = new Random();
+        Scanner scanner = new Scanner(System.in);
+        int numberToGuess = random.nextInt(100) + 1;
+        int guess;
+
+        System.out.println("Guess a number between 1 and 100:");
+
+        do {
+            System.out.print("Enter your guess: ");
+            guess = scanner.nextInt();
+
+            if (guess < numberToGuess) {
+                System.out.println("Too low!");
+            } else if (guess > numberToGuess) {
+                System.out.println("Too high!");
+            } else {
+                System.out.println("Correct! You guessed the number.");
+            }
+        } while (guess != numberToGuess);
+
+        scanner.close();
+    }
+}
